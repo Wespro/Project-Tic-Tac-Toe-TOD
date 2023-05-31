4f9d88d5dfa1364f9d1bc11b3df4dec4158f5cb8
Abdelrahman <Abdelrahmanmohamed5533@yahoo.com>
2023-05-31 23:56:16 +0300

    Add Html and Css.

---
+++ app.js
@@ -0,0 +1,16 @@
+// //variables
+// const squares = document.querySelectorAll(".square");
+// const square = document.querySelector(".square");
+// let lastPlay = "";
+// //Putting markes
+// squares.forEach((item) => {
+//   item.addEventListener("click", function (e) {
+//     if (lastPlay === "X") {
+//       e.target.innerText = "O";
+//       lastPlay = e.target.innerText;
+//     } else {
+//       e.target.innerText = "X";
+//       lastPlay = e.target.innerText;
+//     }
+//   });
+// });
